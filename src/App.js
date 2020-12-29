d1e62b4cf0f9d003fe180ecf2d6659f0cbb20415
averlye <averlye@github.com>
2020-12-29 18:25:21 +0100

    github repository

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       <h1>Weather App</h1>
+      <footer>This project was created by Alicja Nowak. Please find it on <a href="https://github.com/averlye/weather-app-react-final-project.git" target="_blank">GitHub</a>.
+      </footer>
     </div>
   );
 }
