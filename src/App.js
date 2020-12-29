f3c1308e5f29ac653530ec0b950fbe1336a6ea6e
averlye <averlye@github.com>
2020-12-29 18:40:13 +0100

    installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>Weather App</h1>
-      <footer>This project was created by Alicja Nowak. Please find it on <a href="https://github.com/averlye/weather-app-react-final-project.git" target="_blank">GitHub</a>.
-      </footer>
+    <div className="app">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>This project was created by Alicja Nowak. Please find it on <a href="https://github.com/averlye/weather-app-react-final-project.git" target="_blank">GitHub</a>.
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
